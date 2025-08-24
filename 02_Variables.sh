68ba7be4e84754953a1820f75a5059e69f109e8b
sanket2107 <sanketdeshmukh885@gmail.com>
2025-08-24 19:52:22 +0530

    02_Variables.sh file added

---
+++ 02_Variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## In this script we will learn about varibales in shell scripting
+
+# Variables  -- container which hold some values
+## there shoul be no space between variable name and value 
+
+## user defined variables 
+
+name="Tejas"
+age=20
+
+echo "The age of $name is $age."
+
+## Enviormental Variables
+
+## Enviorment variables always written in captial letters
+
+
+echo $USER
+
+echo $SHELL
+
+echo "hello"
